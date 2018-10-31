8e39bcef13663c9f1fe84d10ce3c275bd2eda371
tarik.omanovic1 <tarik.omanovic1@gmail.com>
2018-10-31 17:23:47 +0100

    Finished 10, added 11

--- Chapter10/main.cpp
+++ Chapter10/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int
 next(
@@ -8,12 +10,30 @@ next(
 }
 
 int
-main()
+main(
+	int argc,
+	char **argv)
 {
-	int (*fp)(int);
+	/*int (*fp)(int);
 
 	fp = next;
-	fp = &next;
+	fp = &next;*/
+
+	int i;
+	for (i = 1; i < argc; ++i)
+	{
+		std::cout << argv[i] << std::endl;
+	}
+
+	std::ifstream infile(argv[1]);
+	std::ofstream outfile(argv[2]);
+
+	std::string s;
+
+	while (std::getline(infile, s))
+	{
+		outfile << s << std::endl;
+	}
 
 	return 0;
 }
